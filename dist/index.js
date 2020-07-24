1918d9e6eda4c5a0fb981289b09aab585782d425
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-07-24 23:37:34 +0900

    package

--- dist/index.js
+++ dist/index.js
@@ -6475,7 +6475,7 @@ function mergeFilenames(cov1, cov2) {
     const files1 = cov1.files.map(f => f.filename);
     const files2 = cov2.files.map(f => f.filename);
     const files = new Set([...files1, ...files2]);
-    return Array.from(files);
+    return Array.from(files).sort((a, b) => a < b ? -1 : (a > b ? 1 : 0));
 }
 function isDifference(cov1, cov2) {
     if (cov1 === cov2) {
